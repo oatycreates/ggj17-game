32462d37a79ae26baeb889ea31122533c163867b
Rowan Donaldson <nerdrowan@gmail.com>
2017-01-21 10:57:11 +1100

    Added: Keyboard Shortcuts Script - Reset Scene and Quit Game

---
+++ Assets/Scripts/Game/Game_Shortcuts.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+
+//Allows user to exit game/ reset game on PC/Mac
+public class Game_Shortcuts : MonoBehaviour 
+{
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Debug.Log( "Quit Game");
+			Application.Quit();
+		}
+
+		if (Input.GetKeyDown(KeyCode.Backspace))
+		{
+			Debug.Log ("Reset Scene");
+			SceneManager.LoadScene ( SceneManager.GetActiveScene().name );
+		}
+	}
+}
