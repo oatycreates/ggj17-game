0195b7d13286ea97ac1710dc8fa8af26d4e6bb3a
Rowan Donaldson <nerdrowan@gmail.com>
2017-01-21 15:00:19 +1100

    Changes: Rowan Experiments and Sand Score

---
+++ Assets/Scripts/Game/Game_FindActiveSand.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Find all the active sand blocks in the scene.
+/// </summary>
+public class Game_FindActiveSand : MonoBehaviour 
+{
+	private GameObject[] sand;
+	public List<GameObject> activeSand = new List<GameObject>();
+
+	void Awake()
+	{
+		sand = GameObject.FindGameObjectsWithTag("Sand");
+
+		print ("There are " + sand.Length + " block of sand in the scene.");
+	}
+
+	void Start()
+	{
+		//Add sand to the list ONLY if it's active in heirarchy.
+		for (int i = 0; i < sand.Length; i++)
+		{
+			if (!sand[i].activeInHierarchy)
+			{
+				return;	
+			}
+			else
+			{
+				activeSand.Add( sand[i] );
+			}
+		}
+
+		print ("However, only " + activeSand.Count + " are Active In Heirarchy.");
+	}
+
+}
---
+++ Assets/Scripts/RowanExperiments/SandKillzone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Destroys the Sand as it enters the trigger volume. Make that sand Pay!
+/// </summary>
+public class SandKillzone : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider a_other)
+	{
+		if (a_other.gameObject.tag == "Sand")
+		{
+			a_other.gameObject.SetActive(false);
+		}
+	}
+}
