7877f1ed34f02c7218727d7afe72383c081e8782
Rowan Donaldson <nerdrowan@gmail.com>
2017-01-22 01:16:47 +1100

    Added: New Royalty Free Font and Effect on Scoring Number

--- Assets/Scripts/Game/Game_ScoreSand.cs
+++ Assets/Scripts/Game/Game_ScoreSand.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 /// <summary>
 /// Measures the Number of Active Sand in the Scene Against the amount of 'Destroyed' Sand in the Scene
 /// </summary>
@@ -12,6 +14,15 @@ public class Game_ScoreSand : MonoBehaviour
 
 	private Game_FindRestingSand activeSandCountScript = null;
 
+	public Text scoreText;
+	public UI_Scale uiScale;
+
+	//Use these floats to detect changes in percentage
+	private float currentPercentage = 0;
+	private float lastPercentage = 0;
+	//If a high enough percentage is triggered, then play the special effect.
+	private float effectThreshold = 5;
+
 	void Start()
 	{
 		activeSandCountScript = FindObjectOfType<Game_FindRestingSand>();
@@ -19,7 +30,26 @@ public class Game_ScoreSand : MonoBehaviour
 
 	void Update()
 	{
+		//Check the last percentage to see if there are any changes
+		currentPercentage = ToPercent(activeSandCountScript.GetCastleDestroyPercent());
+		float difference = (Mathf.Abs(lastPercentage - currentPercentage));
+
+		if (difference > effectThreshold)
+		{
+			//Trigger Effect
+			TriggerUIEffect();
+		}
+
+		//Calculate Percentage for Text string
 		percentage = (ToPercent( activeSandCountScript.GetCastleDestroyPercent() ).ToString() + "%");
+
+		if (scoreText != null)
+		{
+			scoreText.text = percentage;
+		}
+
+		//Set the last percentage
+		lastPercentage = currentPercentage;
 	}
 
 	float ToPercent(float a_rawRatio)
@@ -33,10 +63,19 @@ public class Game_ScoreSand : MonoBehaviour
 		return percent;
 	}
 
+	void TriggerUIEffect()
+	{
+		if (uiScale != null)
+		{
+			uiScale.EffectTime(0.5f, true);
+		}
+	}
+
+	/*
 	void OnGUI()
 	{
 		GUI.TextField (new Rect( 35, 30, 50, 20), percentage);
-	}
+	}*/
 
 	/*
 	public static void SandDestroyed()
