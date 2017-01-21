d2df3d3df9c7426451cf77ebf383f31ed323d300
Rowan Donaldson <nerdrowan@gmail.com>
2017-01-21 12:12:43 +1100

    Changes: Further R&D for Sandcastle Physics and Fixed Joints

---
+++ Assets/Scripts/RowanExperiments/MoveWaveWithForce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Move Rigidbody while key is held down
+/// </summary>
+public class MoveWaveWithForce : MonoBehaviour 
+{
+	private Rigidbody myRigid;
+
+	public float moveForce = 1000.0f;
+
+	void Awake()
+	{
+		myRigid = gameObject.GetComponent<Rigidbody>() as Rigidbody;
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			myRigid.AddForce( Vector3.right * moveForce, ForceMode.Acceleration );
+		}
+		else
+		{
+			myRigid.AddForce( Vector3.right * -(moveForce/10), ForceMode.Force);
+		}
+	}
+}
