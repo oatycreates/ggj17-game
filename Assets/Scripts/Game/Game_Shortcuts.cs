8bf20e28e2b3cdb0f0583f7435f10335744439a7
Patrick Ferguson <patrick.ferguson@ancientdawngames.com>
2017-01-22 14:07:42 +1100

    Change quit game shortcut to Delete to not conflict with pause.

--- Assets/Scripts/Game/Game_Shortcuts.cs
+++ Assets/Scripts/Game/Game_Shortcuts.cs
@@ -13,7 +13,7 @@ public class Game_Shortcuts : MonoBehaviour
 
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (Input.GetKeyDown(KeyCode.Delete))
 		{
 			Debug.Log( "Quit Game");
 			Application.Quit();
