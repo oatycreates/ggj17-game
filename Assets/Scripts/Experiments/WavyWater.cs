720eb9f060a8100175ad3537ef25dba2a69fc30f
Patrick Ferguson <patrick.ferguson@ancientdawngames.com>
2017-01-22 13:26:23 +1100

    Implement new water graphic, make wavy.

---
+++ Assets/Scripts/Experiments/WavyWater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class WavyWater : MonoBehaviour {
+	[Range(0.0f, 2.0f)]
+	public float waveSpeed = 1.0f;
+	[Range(0.0f, 1.0f)]
+	public float waveAmplitude = 1.0f;
+	private Renderer m_renderer = null;
+
+	/// <summary>
+    /// Use this for initialization.
+    /// </summary>
+	void Start () {
+		m_renderer = GetComponent<Renderer>();
+	}
+	
+	/// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+	void Update () {
+		float offset = Mathf.Sin(Time.time * waveSpeed) * waveAmplitude;
+        m_renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
+}
