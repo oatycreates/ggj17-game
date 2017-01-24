59e6d852981f6431f1f152cc9d65fd2dbd2b4389
Rowan Donaldson <nerdrowan@gmail.com>
2017-01-24 17:55:03 +1100

    Added: Website Url links and changes to credits structure

---
+++ Assets/Scripts/UI/UI_OpenWebpage.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Trigger these function to open specific Websites via URL
+/// </summary>
+
+public class UI_OpenWebpage : MonoBehaviour 
+{
+	public void GabbyWebpage()
+	{
+		Application.OpenURL("http://3dartist.phoenixinteractive.com.au/");
+	}
+
+	public void PatrickWebpage()
+	{
+		Application.OpenURL("https://github.com/patferguson");
+	}
+
+	public void RowanWebpage()
+	{
+		Application.OpenURL("http://www.rowandonaldson.com/");
+	}
+
+	public void AbbyWebpage()
+	{
+		//Leave empty for now
+	}
+
+
+	public void GabbyTwitter()
+	{
+		//Leave empty for now
+	}
+
+	public void PatrickTwitter()
+	{
+		Application.OpenURL("https://twitter.com/DevPatF");
+	}
+
+	public void RowanTwitter()
+	{
+		Application.OpenURL("https://twitter.com/DevRowan");
+	}
+
+	public void AbbyTwitter()
+	{
+		Application.OpenURL("https://twitter.com/abbysynth");
+	}
+	
+}
