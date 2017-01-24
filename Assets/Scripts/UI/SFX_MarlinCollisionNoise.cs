d364cbbef5e66653c85eed1beaa6c936687c9722
Rowan Donaldson <nerdrowan@gmail.com>
2017-01-24 16:10:08 +1100

    Added: Marlene has SFX on collisions with Sand and Water. SFX are also Incorporated into Options Menu & Audio Mixer

---
+++ Assets/Scripts/UI/SFX_MarlinCollisionNoise.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+/// <summary>
+/// Play an SFX noise on collision with a certain object
+/// </summary>
+
+public class SFX_MarlinCollisionNoise : MonoBehaviour 
+{
+	//Best to assign individual Audio Sources in the editor
+	public AudioSource myAudio;
+	public string tagToSearchFor;
+
+	public float minPitch = 1.0f;
+	public float maxPitch = 1.0f;
+
+	void OnCollisionEnter( Collision a_other )
+	{
+		if (a_other.gameObject.tag == tagToSearchFor)
+		{
+			if (!myAudio.isPlaying)
+			{
+				myAudio.pitch = RandomizePitch( minPitch, maxPitch);
+				myAudio.Play();
+			}
+		}
+	}
+
+	void OnTriggerEnter( Collider a_other )
+	{
+		if (a_other.gameObject.tag == tagToSearchFor)
+		{
+			if (!myAudio.isPlaying)
+			{
+				myAudio.pitch = RandomizePitch( minPitch, maxPitch);
+				myAudio.Play();
+			}
+		}
+	}
+
+	float RandomizePitch( float a_min, float a_max)
+	{
+		float rand = 0;
+
+		rand = Random.Range( a_min, a_max);
+
+		return rand;
+	}
+}
