76156d4ed6d4504caa347aa7c55a54d48d0ab056
Rowan Donaldson <nerdrowan@gmail.com>
2017-02-06 15:38:06 +1100

    Added: SFX to Sand Blocks as they splash into the water. Added: Button SFX when clicked.

---
+++ Assets/Scripts/UI/UI_ButtonNoise.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Plays a Noise on Pointer Over and Pointer Down
+/// </summary>
+
+public class UI_ButtonNoise : MonoBehaviour , IPointerEnterHandler , IPointerDownHandler 
+{
+	public AudioSource sfxEnter;
+	public AudioSource sfxDown;
+
+	public void OnPointerEnter( PointerEventData ped )
+	{
+		//sfxEnter.Play();
+	}
+
+	public void OnPointerDown( PointerEventData ped )
+	{
+		sfxDown.Play();
+	}
+}
---
+++ Assets/Scripts/Water/SFX_WaterSplash.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Plays a splash SFX whenever a sand object enters the water
+/// </summary>
+public class SFX_WaterSplash : MonoBehaviour 
+{
+	public string tagToLookFor = "Sand";
+
+	public AudioSource myAudio;
+
+	public float minPitch = 1.0f;
+	public float maxPitch = 1.0f;
+
+	void OnTriggerEnter( Collider a_other )
+	{
+		if (a_other.gameObject.tag == tagToLookFor)
+		{
+			//if (!myAudio.isPlaying)
+			{
+				myAudio.pitch = RandomizePitch(minPitch, maxPitch);
+				myAudio.Play();
+			}
+		}
+	}
+
+	float RandomizePitch(float a_min, float a_max)
+	{
+		float rand = 0;
+
+		rand = Random.Range(a_min, a_max);
+
+		return rand;
+	}
+	
+}
