f754d56ebe18c3529d8c1dbc0694dd874cac8f64
Gabby Niceforo <metalphoenix@gmail.com>
2017-02-07 14:29:45 +1100

    Added test and art assets to animate

---
+++ Assets/Scripts/Experiments/CloudScrollV3.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*public class CloudScrollV3 : MonoBehaviour{
+	public float scrollSpeed;
+	public float tileSizeZ;
+	private Vector3 startPosition;
+
+	void Start(){
+		startPosition = transform.position;
+	}
+
+	void Update(){
+		float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}*/
+public class CloudScrollV3 : MonoBehaviour{
+	public float scrollSpeed;
+	private Vector2 savedOffset;
+
+	void Start(){
+		savedOffset = GetComponent<Renderer> ().sharedMaterial.GetTextureOffset ("_MainTex");
+	}
+
+	void Update(){
+		float y = Mathf.Repeat (Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2 (y, savedOffset.x);
+		GetComponent<Renderer> ().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	}
+
+	void OnDisable(){
+		GetComponent<Renderer> ().sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+	}
+}
+
+
+
+/*public class CloudScrollV3 : MonoBehaviour {
+	public float scrollSpeed;
+	public float tileSizeZ;
+	private Vector2 savedOffset;
+	private Vector3 startPosition;
+
+	void Start () {
+		startPosition = transform.position;
+		savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset ("_MainTex");
+	}
+
+	void Update(){
+		float x = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ * 4);
+		x = x / tileSizeZ;
+		x = Mathf.Floor (x);
+		x = x / 4;
+		Vector2 offset = new Vector2 (x, savedOffset.y);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+		float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.back * newPosition;
+	}
+
+	void OnDisable(){
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+	}
+}*/
